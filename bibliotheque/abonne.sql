c6ec62051912ddd524ed94c710e2a2be0cc3f47f
nadia karouchi <87656084+nadiakarouchi@users.noreply.github.com>
2021-12-02 17:01:33 +0100

    suite

---
+++ bibliotheque/abonne.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : jeu. 02 déc. 2021 à 16:50
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `bibliotheque`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `abonne`
+--
+
+DROP TABLE IF EXISTS `abonne`;
+CREATE TABLE `abonne` (
+  `id_abonne` int(3) NOT NULL,
+  `prenom` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
